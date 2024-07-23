505033f560417e9a887bd7f8ca8998267a5ed079
rdtyaandr <radltyaa260819@gmail.com>
2024-07-23 13:16:57 +0700

    sdikit perubahan

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,47 +1,25 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-Class Auth extends MY_Controller{
+class Auth extends MY_Controller
+{
 
 	public function __construct()
-{
-	parent::__construct();
-	$this->load->model('m_user');
-	$this->load->library('form_validation');
-	$this->load->model('M_account');
-    
-    $user_id = $this->session->userdata('id');
+	{
+		parent::__construct();
+		$this->load->model('m_user');
+		$this->load->library('form_validation');
+	}
 
-    // Pastikan ada user_id sebelum melanjutkan
-    if ($user_id) {
-        $user_data = $this->M_account->getUserDataById($user_id);
-        if ($user_data) {
-            $this->data['profile_picture'] = $user_data->profile_picture;
-            $this->data['nama_lengkap'] = $this->session->userdata('nama_lengkap');
-            $this->data['username'] = $this->session->userdata('username');
-        } else {
-            // Tangani kasus ketika data pengguna tidak ditemukan
-            $this->data['profile_picture'] = 'default_profile_picture.jpg'; // Atau nilai default lain
-            $this->data['nama_lengkap'] = '';
-            $this->data['username'] = '';
-        }
-    } else {
-        // Jika tidak ada user_id, Anda bisa men-set data default atau menangani kasus ini sesuai kebutuhan
-        $this->data['profile_picture'] = 'default_profile_picture.jpg';
-        $this->data['nama_lengkap'] = '';
-        $this->data['username'] = '';
-    }
-}
-
-	public function index(){
+	public function index()
+	{
 		//jika validasi nya gagal
-		$this->form_validation->set_rules('username','Username','required|trim');
-		$this->form_validation->set_rules('password','Password','required|trim');
-		if($this->form_validation->run() == false)
-		{
+		$this->form_validation->set_rules('username', 'Username', 'required|trim');
+		$this->form_validation->set_rules('password', 'Password', 'required|trim');
+		if ($this->form_validation->run() == false) {
 			$data['title'] = 'Login Page';
 			$this->load->view('auth/v_login', $data);
-		}else{
+		} else {
 			//jika validasinya success
 			$this->_login();
 		}
@@ -51,15 +29,16 @@ Class Auth extends MY_Controller{
 	{
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
-		
-		$user = $this->db->get_where('tb_admin',['username' => $username])->row_array();
-		
-		//jika usernya ada
-		if($user) {
-			//jika usernya aktif
-			if($user['is_active'] ==1 ) {
-				//cek password
-				if(password_verify($password,$user['password'])) {
+
+		// Ambil data pengguna berdasarkan username
+		$user = $this->db->get_where('tb_admin', ['username' => $username])->row_array();
+
+		// Jika pengguna ditemukan
+		if ($user) {
+			// Jika pengguna aktif
+			if ($user['is_active'] == 1) {
+				// Verifikasi password
+				if (password_verify($password, $user['password'])) {
 					$data = [
 						'id' => $user['id'],
 						'nama_lengkap' => $user['nama_lengkap'],
@@ -68,73 +47,83 @@ Class Auth extends MY_Controller{
 						'role_id' => $user['role_id']
 					];
 					$this->session->set_userdata($data);
+
+					// Arahkan berdasarkan role_id
 					if ($user['role_id'] == 1) {
 						redirect('dashboard');
-					} else {
+					} elseif ($user['role_id'] == 2) {
+						redirect('dashboard');
+					} elseif ($user['role_id'] == 3) {
 						redirect('user/user');
+					} else {
+						// Default redirect jika role_id tidak dikenali
+						redirect('auth');
 					}
-
-				}else {
-				$this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
-				Wrong Password!
-			  </div>');
-				redirect('auth');
-			}
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Wrong Password!
+                </div>');
+					redirect('auth');
+				}
 			} else {
-				$this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
-				Your Account has not been activated
-			  </div>');
+				$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Your Account has not been activated
+            </div>');
 				redirect('auth');
 			}
 		} else {
-			$this->session->set_flashdata('message','<div class="alert alert-danger" role="alert">
-			Your user not found
-		  </div>');
+			$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+        Your user not found
+        </div>');
 			redirect('auth');
 		}
 	}
 
 	public function register()
 	{
-		$this->form_validation->set_rules('username','Username','required|trim');
-		$this->form_validation->set_rules('nama_lengkap','Fullname','required|trim');
-		$this->form_validation->set_rules('password1','Password','required|trim|min_length[3]|matches[password2]',[
+		$this->form_validation->set_rules('username', 'Username', 'required|trim');
+		$this->form_validation->set_rules('nama_lengkap', 'Fullname', 'required|trim');
+		$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
 			'matches' => 'Password dont match!',
 			'min_lenght' => 'Password Too Short!'
 		]);
-		$this->form_validation->set_rules('password2','Password','required|trim|matches[password1]');
+		$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
 
-		if($this->form_validation->run() == false) {
-			$data ['title'] ='Koperasi Registration';
+		if ($this->form_validation->run() == false) {
+			$data['title'] = 'Koperasi Registration';
 			$this->load->view('auth/v_regist', $data);
-		}else{
+		} else {
 			$data = [
 				'username' => htmlspecialchars($this->input->post('username')),
 				'nama_lengkap' => htmlspecialchars($this->input->post('nama_lengkap')),
 				'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-				'role_id' => 2,
+				'role_id' => 3,
 				'profile_picture' => 'material/image/user.jpg',
 				'is_active' => 1
 			];
-			$this->db->insert('tb_admin',$data);	
-			$this->session->set_flashdata('message','<div class="alert alert-success" role="alert">
+			$this->db->insert('tb_admin', $data);
+			$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
 			Congratulation! your account is been created. please login!
-		  </div>');
+		</div>');
 			redirect('auth');
 		}
 	}
 
 
 
-		public function logout()
+	public function logout()
 	{
-		$params = ['id','username','role_id'];
+		// Hapus data sesi
 		$this->session->sess_destroy();
-		$this->session->unset_userdata($params);
-		$this->session->set_flashdata('message','<div class="alert alert-primary" role="alert">
-		You has been Logout!
-	  </div>');
+
+		// Hapus semua userdata
+		$this->session->unset_userdata(['id', 'username', 'role_id']);
+
+		// Atur pesan dan arahkan ulang ke halaman login
+		$this->session->set_flashdata('message', '<div class="alert alert-primary" role="alert">
+    You have been logged out!
+    </div>');
 		redirect('auth');
 	}
 
@@ -142,18 +131,17 @@ Class Auth extends MY_Controller{
 	{
 		$post = $this->input->post(null, TRUE);
 
-		if(isset($post['login'])){
+		if (isset($post['login'])) {
 			//ambil data dari tabel dengan model
 			$this->load->model('m_user');
 
 			$query = $this->m_user->login($post);
 
 			//validasi
-			if($query->num_rows() > 0)
-			{
+			if ($query->num_rows() > 0) {
 				$row = $query->row();
 				$params = array(
-					'id'=> $row->id,
+					'id' => $row->id,
 					'nama_lengkap' => $row->nama_lengkap,
 					'username' => $row->username,
 				);
@@ -169,24 +157,25 @@ Class Auth extends MY_Controller{
 		}
 	}
 
-	public function update_account() {
-    $this->form_validation->set_rules('nama_lengkap', 'Nama Lengkap', 'required');
-    $this->form_validation->set_rules('username', 'Username', 'required');
-    $this->form_validation->set_rules('password', 'Password', 'required');
-    $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
+	public function update_account()
+	{
+		$this->form_validation->set_rules('nama_lengkap', 'Nama Lengkap', 'required');
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('confirm_password', 'Confirm Password', 'required|matches[password]');
 
-    if ($this->form_validation->run() == FALSE) {
-        $this->session->set_flashdata('error', 'Update account failed');
-        redirect(base_url('dashboard/account_setting'));
-    } else {
-        $data = array(
-            'nama_lengkap' => $this->input->post('nama_lengkap'),
-            'username' => $this->input->post('username'),
-            'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
-        );
-        $this->session->set_userdata($data);
-        $this->session->set_flashdata('success', 'Update account successful');
-        redirect(base_url('dashboard/account_setting'));
-    }
-}
+		if ($this->form_validation->run() == FALSE) {
+			$this->session->set_flashdata('error', 'Update account failed');
+			redirect(base_url('dashboard/account_setting'));
+		} else {
+			$data = array(
+				'nama_lengkap' => $this->input->post('nama_lengkap'),
+				'username' => $this->input->post('username'),
+				'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+			);
+			$this->session->set_userdata($data);
+			$this->session->set_flashdata('success', 'Update account successful');
+			redirect(base_url('dashboard/account_setting'));
+		}
+	}
 }
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,12 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class MY_Controller extends CI_Controller {
+class MY_Controller extends CI_Controller
+{
     protected $data = [];
     protected $user_data = null;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
+        $this->check_login();
 
         // Memuat model
         $this->load->model('M_account');
@@ -14,7 +17,6 @@ class MY_Controller extends CI_Controller {
         // Mendapatkan ID pengguna dari session
         $user_id = $this->session->userdata('id');
 
-        // Jika ada user_id, ambil data pengguna
         if ($user_id) {
             $this->user_data = $this->M_account->getUserDataById($user_id);
             
@@ -24,15 +26,40 @@ class MY_Controller extends CI_Controller {
                 $this->data['username'] = $this->session->userdata('username');
             } else {
                 // Tangani kasus ketika data pengguna tidak ditemukan
-                $this->data['profile_picture'] = 'default_profile_picture.jpg';
+                $this->data['profile_picture'] = 'material/image/user.jpg';
                 $this->data['nama_lengkap'] = '';
                 $this->data['username'] = '';
             }
         } else {
             // Jika tidak ada user_id, set data default
-            $this->data['profile_picture'] = 'default_profile_picture.jpg';
+            $this->data['profile_picture'] = 'material/image/user.jpg';
             $this->data['nama_lengkap'] = '';
             $this->data['username'] = '';
+        }
+    }
+
+    protected function check_login()
+    {
+        $controller = $this->router->fetch_class();
+        $method = $this->router->fetch_method();
+        
+        // Jika pengguna belum login, arahkan ke halaman login
+        if (!$this->session->userdata('id')) {
+            if ($controller !== 'auth') {
+                redirect('auth');
+            }
+        } else {
+            // Jika pengguna sudah login, arahkan ke halaman sesuai role_id
+            if ($controller === 'auth' && $method !== 'logout') {
+                $role_id = $this->session->userdata('role_id');
+                if ($role_id == 1) {
+                    redirect('dashboard');
+                } elseif ($role_id == 2) {
+                    redirect('dashboard');
+                }elseif ($role_id == 3) {
+                    redirect('user/user');
+                }
+            }
         }
     }
 }
